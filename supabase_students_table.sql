6296b0d6248539b1846002dd2fe345effd82635e
sandada035-blip <sandada035@gmail.com>
2026-03-16 10:36:04 +0700

    Delete supabase_students_table.sql

--- supabase_students_table.sql
+++
@@ -1,2 +0,0 @@
-create extension if not exists "uuid-ossp";
-create table if not exists students (id uuid primary key default uuid_generate_v4(), name text, gender text, grade text, teacher text, created_at timestamp default now());
